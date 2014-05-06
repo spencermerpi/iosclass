4408a6519f2f24734e7c4200ad795368537fe2dd
spencer <spencer.merpi@gmail.com>
2014-05-06 18:21:31 -0400

    Initial Commit

---
+++ oopisannoying/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  oopisannoying
+//
+//  Created by Spencer Merpi on 5/6/14.
+//  Copyright (c) 2014 Spencer Merpi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
